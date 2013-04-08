c12d432dab4b806c3d842e1e234e3ff39918dc43
Tim Watts <tim@readevalprint.com>
2013-04-08 03:17:10 +0000

    Cleaned up settings, loads an ignored local_settings.py

--- billy/billy/settings.py
+++ billy/billy/settings.py
@@ -1,5 +1,11 @@
 # Django settings for billy project.
 
+import os
+import djcelery
+djcelery.setup_loader()
+
+here = lambda x: os.path.join(os.path.abspath(os.path.dirname(__file__)), x)
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -11,26 +17,21 @@ MANAGERS = ADMINS
 
 DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-            'NAME': 'db',                      # Or path to database file if using sqlite3.
-            # The following settings are not used with sqlite3:
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': here('db'),
             'USER': '',
             'PASSWORD': '',
-            'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
-            'PORT': '',                      # Set to empty string for default.
+            'HOST': '',
+            'PORT': '',
             }
         }
 
-import os
 
 BALANCED = {
         'API_KEY': os.environ.get('BALANCED_API_KEY'),
         }
 
-BROKER_URL = 'amqp://koflwbpt:H6EPt6dYmHLZegMV4IGWqbILBCHFYhXc@tiger.cloudamqp.com/koflwbpt'
-BROKER_BACKEND = 'memory'
-CELERY_ALWAYS_EAGER = True
-
+BROKER_URL = ''
 
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
@@ -62,18 +63,18 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = here('media_root')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = here('static_root')
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -81,9 +82,7 @@ STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
-        # Put strings here, like "/home/html/static" or "C:/www/django/static".
-        # Always use forward slashes, even on Windows.
-        # Don't forget to use absolute paths, not relative paths.
+        here('static'),
         )
 
 # List of finder classes that know how to find static files in
@@ -101,7 +100,7 @@ SECRET_KEY = 'ehw4j+ow4d#*ce(!_rgrc$n#1$=9)$ys@b99)k7n+8dna^x()8'
 TEMPLATE_LOADERS = (
         'django.template.loaders.filesystem.Loader',
         'django.template.loaders.app_directories.Loader',
-        #     'django.template.loaders.eggs.Loader',
+        'django.template.loaders.eggs.Loader',
         )
 
 MIDDLEWARE_CLASSES = (
@@ -148,31 +147,7 @@ INSTALLED_APPS = (
         'south', # Keep at end
         )
 
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error when DEBUG=False.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
-LOGGING = {
-        'version': 1,
-        'disable_existing_loggers': False,
-        'filters': {
-            'require_debug_false': {
-                '()': 'django.utils.log.RequireDebugFalse'
-                }
-            },
-        'handlers': {
-            'mail_admins': {
-                'level': 'ERROR',
-                'filters': ['require_debug_false'],
-                'class': 'django.utils.log.AdminEmailHandler'
-                }
-            },
-        'loggers': {
-            'django.request': {
-                'handlers': ['mail_admins'],
-                'level': 'ERROR',
-                'propagate': True,
-                },
-            }
-        }
+try:
+    from local_settings import *
+except:
+    print "No local_settings.py"
